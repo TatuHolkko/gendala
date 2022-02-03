926864ba25ddd3f12bd1301db5c025cc32ce704b
Tatu Holkko <tatu.holkko@gmail.com>
2022-02-03 21:31:41 +0200

    add chance to generate a border between layers

--- generator/generator.py
+++ generator/generator.py
@@ -1,8 +1,9 @@
 import random
-from curve import GeometryException
+from curve import Curve, GeometryException
+from geometry import Point
 from ribbon import Ribbon
 from feature import Feature
-from generator.patternGenerators import randomPattern
+from generator.patternGenerators import centerLine, randomPattern
 from generator.curveGenerators import randomCurve
 from utility import coinFlip
 
@@ -17,6 +18,10 @@ class Generator:
         n = random.choice([1,1,2])
         for _ in range(n):
             feature.add(self.getRibbon())
+        if coinFlip() or coinFlip():
+            line = Curve(Point(-1,1))
+            line.extend(line.line(Point(1,1)))
+            feature.add(Ribbon(curve=line, pattern=centerLine(), closed=False, width=1))
         return feature
 
 
